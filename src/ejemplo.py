495fcff01a4c799b4bd5f1b2621fa030aa778e41
jnaftaly <joaquin.naftaly@gmail.com>
2020-07-19 19:28:49 -0300

    sanger arreglado

--- src/ejemplo.py
+++ src/ejemplo.py
@@ -3,5 +3,8 @@ from hebbiano import Hebbiano
 hebbiano = Hebbiano('./dataset/tp2_training_dataset.csv')
 
 # alg, M, lr, min_ort, max_epoch, trace (= 0)
-hebbiano.train('oja', 9, 0.001, 0.00001, 600, 1)
+#hebbiano.train('oja', 9, 0.00001, 0.001, 600, 1)
+#hebbiano.plot()
+
+hebbiano.train('sanger', 9, 0.001, 0.001, 600, 1)
 hebbiano.plot()
--- src/hebbiano.py
+++ src/hebbiano.py
@@ -14,11 +14,9 @@ def regla_oja(X, W, h, lr):
 def regla_sanger(X, W, M, h, lr):
     y = np.dot(X[h], W)
     d = np.triu(np.ones((M, M)))
-    z = np.dot( W, y.T*d)
-    _x = np.vstack((X[h].T, X[h].T, X[h].T, X[h].T, X[h].T, X[h].T, X[h].T, X[h].T, X[h].T))
-    return (_x.T - z) * y
-    #z = np.dot(W, np.dot(y.T,d))
-    #dw = lr*np.outer(X[h].T - z,y)
+    z = np.dot( W, np.array([y]).T*d)
+    return lr * (np.array([X[h]]).T - z) * y
+
 
 class Hebbiano:
     def __init__(self, dataset_dir):
@@ -38,6 +36,12 @@ class Hebbiano:
             o = ortogonalidad(W, M)
             if (trace != 0 and t % trace == 0): print(o)
 
+            #sanger lr adaptative.
+            if(alg == 'sanger'):
+                if(o < 4):
+                    if t % 30 == 0:
+                        lr=0.001/t
+
             for h in range(self.P):
                 if alg == 'oja':
                     W += regla_oja(self.X, W, h, lr)
@@ -55,9 +59,7 @@ class Hebbiano:
         #Grafico
         fig = mpl.figure()
         xyz = fig.add_subplot(131, projection='3d')
-        xyz.scatter3D(y[:,0], y[:,1], y[:,2], 'b.', c=y[:,9], s=7, alpha=0.1)
-        xyz.plot([0, self.W[0][0]], [0, self.W[0][1]], [0, self.W[0][2]], 'r-')
-
+        xyz.scatter3D(y[:,0], y[:,1], y[:,2], 'b.', c=y[:,9], s=7)
 
         xyz2 = fig.add_subplot(132, projection='3d')
         xyz2.scatter3D(y[:,3], y[:,4], y[:,5], 'b.', c=y[:,9], s=7)
