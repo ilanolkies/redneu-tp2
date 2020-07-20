8dce460518caee0f5fcee968b283f3daadefe0f1
jnaftaly <joaquin.naftaly@gmail.com>
2020-07-20 19:36:32 -0300

    graphics and models

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ sys.path.insert(1, './src')
 from os import path
 from dataset import load_dataset
 from hebbiano import Hebbiano
-from plotter import plot
+from plotter import plot, plot_error
 
 nombre_modelo = sys.argv[1]
 dataPath = sys.argv[2]
@@ -14,11 +14,15 @@ if not path.exists(nombre_modelo + '.p'):
 
   alg = sys.argv[3]
   if alg == 'oja':
-    modelo.train('oja', 9, 0.001, 0.001, 600, 100) # TBD
+    errors = modelo.train('oja', 9, 0.00001, 0.0001, 1500, 100) 
   elif alg == 'sanger':
-    modelo.train('oja', 9, 0.001, 0.001, 600, 100) # TBD
+    errors = modelo.train('sanger', 9, 0.00001, 0.001, 2000, 100) #0.04134266836522473
 
   modelo.save(nombre_modelo + '.p')
+  
+  plot_error(errors)
+  plot(modelo.test(data), labels)
+  
 else:
   modelo = Hebbiano()
   modelo.load(nombre_modelo + '.p')
--- src/plotter.py
+++ src/plotter.py
@@ -7,16 +7,27 @@ def plot(y, labels, s = 5):
 
     xyz = fig.add_subplot(131, projection='3d')
     xyz.scatter3D(y[:,0], y[:,1], y[:,2], 'b.', c=labels[:,0], s=s)
+    xyz.set_xlabel('$Y_1$')
+    xyz.set_ylabel('$Y_2$')
+    xyz.set_zlabel('$Y_3$')
 
     xyz2 = fig.add_subplot(132, projection='3d')
     xyz2.scatter3D(y[:,3], y[:,4], y[:,5], 'b.', c=labels[:,0], s=s)
+    xyz2.set_xlabel('$Y_4$')
+    xyz2.set_ylabel('$Y_5$')
+    xyz2.set_zlabel('$Y_6$')
 
     xyz3 = fig.add_subplot(133, projection='3d')
     xyz3.scatter3D(y[:,6], y[:,7], y[:,8], 'b.', c=labels[:,0], s=s)
+    xyz3.set_xlabel('$Y_7$')
+    xyz3.set_ylabel('$Y_8$')
+    xyz3.set_zlabel('$Y_9$')
 
     plt.show()
 
 def plot_error(errors):
     plt.title('Errores')
+    plt.xlabel('Epochs')
+    plt.ylabel('Ortogonalidad')
     plt.plot(errors)
     plt.show()
