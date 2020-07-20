f3b07771ea8df5764a06903a277e6feb9f48df94
Ilan Olkies <ilanolkies@outlook.com>
2020-07-20 17:34:41 -0300

    Ejemplo oja

---
+++ src/ejemplo_oja.py
@@ -0,0 +1,19 @@
+from dataset import load_dataset
+from hebbiano import Hebbiano
+from plotter import plot, plot_error
+
+data, labels = load_dataset('./dataset/tp2_training_dataset.csv')
+
+data_train = data[:700]
+labels_train = labels[:700]
+
+data_test = data[700:]
+labels_test = labels[700:]
+
+modelo = Hebbiano(data_train)
+
+errors = modelo.train('oja', 9, 0.00001, 0.0001, 10000, 100)
+
+plot_error(errors)
+plot(modelo.test(data_train), labels_train)
+plot(modelo.test(data_test), labels_test)
