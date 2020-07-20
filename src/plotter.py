5e9450cdb68568d15358aadf5a475ce3d916aebf
Ilan Olkies <ilanolkies@outlook.com>
2020-07-20 01:10:45 -0300

    Prueba ejemplo

--- src/plotter.py
+++ src/plotter.py
@@ -1,9 +1,9 @@
 import numpy as np
-from matplotlib import pyplot as mpl
+from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def plot(y, labels, s = 5):
-    fig = mpl.figure()
+    fig = plt.figure()
 
     xyz = fig.add_subplot(131, projection='3d')
     xyz.scatter3D(y[:,0], y[:,1], y[:,2], 'b.', c=labels[:,0], s=s)
@@ -14,4 +14,9 @@ def plot(y, labels, s = 5):
     xyz3 = fig.add_subplot(133, projection='3d')
     xyz3.scatter3D(y[:,6], y[:,7], y[:,8], 'b.', c=labels[:,0], s=s)
 
-    mpl.show()
+    plt.show()
+
+def plot_error(errors):
+    plt.title('Errores')
+    plt.plot(errors)
+    plt.show()
